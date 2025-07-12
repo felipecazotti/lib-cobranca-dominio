ec9da0cef6df38a448759fc1dae571d8ff5c1162
felipecazotti <felipecazotti@gmail.com>
2025-07-12 17:52:52 -0300

    feat: adiciona modelo de resposta e excecao

---
+++ Cobranca.Lib.Dominio/Exceptions/RegraNegocioException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using Cobranca.Lib.Dominio.Models;
+
+namespace Cobranca.Lib.Dominio.Exceptions;
+
+public class RegraNegocioException(HttpStatusCode httpStatusCode, string codigoErro, string mensagemErro, Exception? originalException = null) 
+    : Exception(mensagemErro, originalException)
+{
+    public HttpStatusCode StatusCode { get; set; } = httpStatusCode;
+    public ResponseModel ResponseModel { get; set; } = new()
+    {
+        Codigo = codigoErro,
+        Messagem = mensagemErro
+    };
+
+}
---
+++ Cobranca.Lib.Dominio/Models/ResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Cobranca.Lib.Dominio.Models;
+
+public class ResponseModel<T> : ResponseModel
+{
+    public T? Data { get; set; }
+}
+
+public class ResponseModel
+{
+    public required string Codigo { get; set; }
+    public string? Messagem { get; set; }
+}
